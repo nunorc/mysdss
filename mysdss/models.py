d08d29b147acacd4e8e28c5eaab52a9cfbee73d0
Nuno Carvalho <nuno.carvalho@cm-guimaraes.pt>
2022-03-09 11:39:12 +0000

    Add model

--- mysdss/models.py
+++ mysdss/models.py
@@ -238,3 +238,14 @@ def w2r(norm):
     model = tf.keras.Model(inputs=wise, outputs=redshift, name='w2r')
 
     return model
+
+def w2sm(norm):
+    wise = tf.keras.Input(shape=(4), name='wise')
+    x = norm(wise)
+    x = tf.keras.layers.Dense(32, activation='relu')(x)
+    x = tf.keras.layers.Dense(32, activation='relu')(x)
+    smass = tf.keras.layers.Dense(1, activation='linear', name='smass')(x)
+
+    model = tf.keras.Model(inputs=wise, outputs=smass, name='b2sm')
+
+    return model
