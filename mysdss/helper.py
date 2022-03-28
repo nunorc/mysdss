dba185e77e5edea8926d0ca4e29a326458b296f5
Nuno Carvalho <nuno.carvalho@cm-guimaraes.pt>
2022-03-28 17:11:19 +0100

    Start moving away from mongodb

--- mysdss/helper.py
+++ mysdss/helper.py
@@ -4,58 +4,33 @@ import numpy as np
 from pandas import read_csv
 from tqdm import tqdm
 import tensorflow.keras.preprocessing.image as keras
-import pymongo
 
 logger = logging.getLogger(__name__)
 
-from .cache import Cache
-
-def _guess_files(FILES):
-    if os.path.exists(FILES):
-        return FILES
-    tmp = os.path.join('.', FILES.split('/')[-1])
-    if os.path.exists(tmp):
-        return tmp
-    tmp = os.path.join('..', FILES.split('/')[-1])
-    if os.path.exists(tmp):
-        return tmp
-    
-    logger.warning('FILES not found: '+FILES)
-    return None
-
-
 class Helper():
-    def __init__(self, FILES='../../FILES', mongodb='mongodb://localhost:27017/', col='sdss', cache=True):
-        self.FILES = _guess_files(FILES)
-        
-        if mongodb:
-            self.client = pymongo.MongoClient(mongodb)
-            self.db = self.client['astro']
-            self.col = self.db[col]
+    def __init__(self, ds='../sdss-gs'):
+        self.FILES = ds
 
-        if cache:
-            self.cache = Cache()
+        if not os.path.exists(self.FILES):
+            logger.warn(f'Dataset files directory not found: { self.FILES }')
+
+        self.df = None
+        _filename = os.path.join(self.FILES, 'data.csv')
+        if os.path.exists(_filename):
+            self.df = read_csv(_filename)
         else:
-            self.cache = None
+            logger.warn(f'Data file not found: { _filename }')
 
     def ids_list(self, has_img=False, has_fits=False, has_spectra=False, has_ssel=False, has_bands=False, has_wise=False, has_gz2c=False):
-        if self.cache:
-            key = self._ids_list_key(has_img, has_fits, has_spectra, has_ssel, has_bands, has_wise, has_gz2c)
-            data = self.cache.get(key)
-
-            if data:
-                return data
-
-        q = {}
+        _df = self.df.copy()
 
         if has_bands:
-            q['bands'] = { '$exists': 1 }
+            _df = _df[~_df['modelMag_u'].isna() & ~_df['modelMag_g'].isna() & ~_df['modelMag_r'].isna() & ~_df['modelMag_i'].isna() & ~_df['modelMag_z'].isna()]
         if has_wise:
-            q['wise'] = { '$exists': 1 }
+            _df = _df[~_df['w1mag'].isna() & ~_df['w2mag'].isna() & ~_df['w3mag'].isna() & ~_df['w4mag'].isna()]
         if has_gz2c:
-            q['gz2class'] = { '$exists': 1 }
-
-        _ids = [x['_id'] for x in self.col.find(q, { '_id': 1 })]
+            _df = _df[~_df['gz2c_f'].isna() & ~_df['gz2c_s'].isna()]
+        _ids = _df['objid'].tolist()
 
         if has_img:
             _ids = [x for x in _ids if self._has_img(x)]
@@ -66,13 +41,7 @@ class Helper():
         if has_ssel:
             _ids = [x for x in _ids if self._has_ssel(x)]
 
-        if self.cache:
-            self.cache.set(key, _ids)
-
         return _ids
-
-    def _ids_list_key(self, *args):
-        return f'ids_list_{ self.col.name }_' + "_".join([str(x) for x in args])
 
     def _has_img(self, _id):
         return os.path.exists(self.img_filename(_id))
@@ -91,12 +60,37 @@ class Helper():
         else:
             return False
 
+    def _has_ssel(self, id):
+        return os.path.exists(self.ssel_filename(id))
+
+    def img_filename(self, objID, DIR='img'):
+        return os.path.join(self.FILES, DIR, str(objID)+'.jpg')
+
+    def fits_filename(self, objID, DIR='fits'):
+        return os.path.join(self.FILES, DIR, str(objID)+'.npy')
+
+    def spectra_filename(self, objID, DIR='spectra'):
+        return os.path.join(self.FILES, DIR, str(objID)+'.csv')
+
+    def ssel_filename(self, objID, DIR='ssel'):
+        return os.path.join(self.FILES, DIR, str(objID)+'.csv')
+
+    def get_row(self, id):
+        if isinstance(id, str):
+            id = int(id)
+
+        sl = self.df[self.df['objid'] == id]
+        if sl.empty:
+            return None
+        else:
+            return sl.iloc[0].to_dict()
+
     def y_list(self, ids, target):
         if target in ['redshift', 'stellarmass']:
             res = []
             for i in ids:
-                d = self.col.find_one({ '_id': i }, { target: 1 })
-                res.append(d[target])
+                row = self.get_row(i)
+                res.append(row[target])
 
             return res
 
@@ -105,42 +99,23 @@ class Helper():
 
         y = []
         for i in ids:
-            o = self.get_obj(i)
+            _row = self.get_row(i)
             if target == 'gz2c':     # exception for gz2class
-                c = o['gz2class']['s']
+                c = _row['gz2c_s']
             else:
-                c = o[target]
+                c = _row[target]
             tmp = np.zeros(n)
             tmp[classes.index(c)] = 1
             y.append(tmp)
 
         return y, classes
 
-    def get_obj(self, _id):
-        if isinstance(_id, str):
-            _id = int(_id)
 
-        return self.col.find_one({ '_id': _id })
+    # def spectra_url(self, objid):
+    #     obj = self.get_obj(objid)
 
-    def img_filename(self, objID, DIR='sdss-img-galaxy'):
-        d = str(objID)[-1]
-        os.makedirs(os.path.join(self.FILES, DIR, d), exist_ok=True)
-        filename = os.path.join(self.FILES, DIR, d, str(objID)+'.jpg')
-        
-        return filename
-
-    def spectra_filename(self, objID, DIR='sdss-spectra-galaxy'):
-        d = str(objID)[-1]
-        os.makedirs(os.path.join(self.FILES, DIR, d), exist_ok=True)
-        filename = os.path.join(self.FILES, DIR, d, str(objID)+'.csv')
-        
-        return filename
-
-    def spectra_url(self, objid):
-        obj = self.get_obj(objid)
-
-        return f"https://dr16.sdss.org/optical/spectrum/view/data/format=csv/spec=lite?plateid={ obj['plate'] }&mjd={ obj['mjd'] }&fiberid={ obj['fiberid'] }"
-        #return f"https://dr17.sdss.org/optical/spectrum/view/data/format=csv/spec=lite?plateid={ obj['plate'] }&mjd={ obj['mjd'] }&fiberid={ obj['fiberid'] }"
+    #     return f"https://dr16.sdss.org/optical/spectrum/view/data/format=csv/spec=lite?plateid={ obj['plate'] }&mjd={ obj['mjd'] }&fiberid={ obj['fiberid'] }"
+    #     #return f"https://dr17.sdss.org/optical/spectrum/view/data/format=csv/spec=lite?plateid={ obj['plate'] }&mjd={ obj['mjd'] }&fiberid={ obj['fiberid'] }"
 
     def load_imgs(self, _ids):
         X_img = []
@@ -173,12 +148,23 @@ class Helper():
 
         return np.array(X_spectra)
 
+    def load_ssels(self, _ids):
+        X_ssel = []
+
+        for i in _ids:
+            _df = read_csv(self.ssel_filename(i))
+            x = _df['BestFit'].to_numpy()
+            X_ssel.append(x)
+
+        return np.array(X_ssel)
+
     def load_bands(self, _ids):
         X_bands = []
 
         for i in _ids:
-            o = self.get_obj(i)
-            X_bands.append(o['bands'])
+            row = self.get_row(i)
+            x = [row['modelMag_u'], row['modelMag_g'], row['modelMag_r'], row['modelMag_i'], row['modelMag_z']]
+            X_bands.append(x)
 
         return np.array(X_bands)
 
@@ -186,15 +172,17 @@ class Helper():
         X_wise = []
 
         for i in _ids:
-            o = self.get_obj(i)
-            X_wise.append(o['wise'])
+            row = self.get_row(i)
+            x = [row['w1mag'], row['w2mag'], row['w3mag'], row['w4mag']]
+            X_wise.append(x)
 
         return np.array(X_wise)
+
 
     def _frame_url(self, obj, band):
         return f"https://dr17.sdss.org/sas/dr17/eboss/photoObj/frames/{ obj['rerun'] }/{ obj['run'] }/{ obj['camcol'] }/frame-{ band }-{ str(obj['run']).zfill(6) }-{ obj['camcol'] }-{ str(obj['field']).zfill(4) }.fits.bz2"
 
-    def _frame_filename(self, obj, band, DIR='sdss-frames-galaxy', bz=False):
+    def _frame_filename(self, obj, band, DIR='frames', bz=False):
         d = os.path.join(self.FILES, DIR, str(obj['rerun']), str(obj['run']))
         os.makedirs(d, exist_ok=True)
         filename = os.path.join(d, str(obj['objid']) + '_' + band + '.fits')
@@ -205,7 +193,7 @@ class Helper():
         return filename
 
     def frames_urls_filenames(self, _id):
-        obj = self.get_obj(_id)
+        obj = self.get_row(_id)
 
         if obj:
             r = []
@@ -217,36 +205,10 @@ class Helper():
 
         return r
 
-    def fits_filename(self, objID, DIR='sdss-fits-galaxy'):
-        d = str(objID)[-1]
-        os.makedirs(os.path.join(self.FILES, DIR, d), exist_ok=True)
-        filename = os.path.join(self.FILES, DIR, d, str(objID)+'.npy')
-        
-        return filename
-
     def random_id(self):
-        _ids = [x['_id'] for x in self.col.find({}, { '_id': 1 })]
+        _ids = self.df['objid'].tolist()
 
         return random.choice(_ids)
 
-    def ssel_filename(self, objID, DIR='sdss-ssel-galaxy'):
-        d = str(objID)[-1]
-        os.makedirs(os.path.join(self.FILES, DIR, d), exist_ok=True)
-        filename = os.path.join(self.FILES, DIR, d, str(objID)+'.csv')
-
-        return filename
-
-    def _has_ssel(self, id):
-        return os.path.exists(self.ssel_filename(id))
-
-    def load_ssels(self, _ids):
-        X_ssel = []
-
-        for i in _ids:
-            _df = read_csv(self.ssel_filename(i))
-            x = _df['BestFit'].to_numpy()
-            X_ssel.append(x)
-
-        return np.array(X_ssel)
 
 
